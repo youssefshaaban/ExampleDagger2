4685af6f244ad1d20eb6296d0663eaef7d82295c
yousef <yousefshaaban0@gmail.com>
2022-11-03 13:54:31 +0200

    remove coffee shop

--- app/src/main/java/com/example/dagger/MainActivity.kt
+++ app/src/main/java/com/example/dagger/MainActivity.kt
@@ -9,24 +9,12 @@ import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
-
-    //    @Inject
-//    lateinit var coffee: Coffee
-//    @Inject
-//    lateinit var coffee2: Coffee
     @Inject
     lateinit var care: Care
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        DaggerCareComponent.builder().bindHorseBower(140).build().inject(this)
+        DaggerCareComponent.builder().bindHorseBower(140).bindCapacity(200).build().inject(this)
         care.drive()
-//        val appComponent=(application as com.example.dagger.MyApp).appComponent
-//        val coffeeComponent=appComponent.getCoffeeCompanent().milk(4).sugar(4).builder()
-//        coffeeComponent.inject(this)
-//        Log.e(TAG, "yousef on create " + coffee.getCoffeeCup()+
-//                "\ncoffee 1 $coffee"
-//                +"\ncoffee2 $coffee2"+
-//                "\nfarm for coffee1 ${coffee.river}" +"\nfarm for coffee2 ${coffee2.river}")
     }
 }
