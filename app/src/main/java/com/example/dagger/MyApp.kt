9d7987a850f1bc7a1a4e64f7c61da2f200a5802c
yousef <yousefshaaban0@gmail.com>
2022-11-03 13:52:25 +0200

    remove coffee shop

--- app/src/main/java/com/example/dagger/MyApp.kt
+++ app/src/main/java/com/example/dagger/MyApp.kt
@@ -1,13 +1,9 @@
 package com.example.dagger
 
 import android.app.Application
-import com.example.dagger.coffe_shope.AppComponent
-import com.example.dagger.coffe_shope.DaggerAppComponent
 
 class MyApp : Application() {
-    lateinit var appComponent: AppComponent
     override fun onCreate() {
         super.onCreate()
-        appComponent = DaggerAppComponent.create()
     }
 }
