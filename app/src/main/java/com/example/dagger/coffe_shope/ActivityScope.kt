fb52f26cb3f90940041fd843c3cf9b175b1fa811
yousef <yousefshaaban0@gmail.com>
2022-11-03 13:51:49 +0200

    remove coffee shop

--- app/src/main/java/com/example/dagger/coffe_shope/ActivityScope.kt
+++
@@ -1,8 +0,0 @@
-package com.example.dagger.coffe_shope
-
-import javax.inject.Scope
-
-@Scope
-@MustBeDocumented
-@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
-annotation class ActivityScope()
